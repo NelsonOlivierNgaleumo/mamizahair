09cc71bb319542939c8269ee8825f2851beb4fd9
Nelson Ngaleumo <nelsonngaleumo@yahoo.fr>
2024-09-07 10:17:20 -0500

    firebase env

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,8 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.displayName}! </h1>
+      <h1>Hello {user.displayName}!</h1>
+      <h2>Welcome to MamizaHair Store</h2>
       <p>Click the button below to logout!</p>
       <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
         Sign Out
