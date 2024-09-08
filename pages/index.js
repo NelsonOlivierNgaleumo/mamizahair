8f82088787b983309f9d950b5093fd3f96f6318c
Nelson Ngaleumo <nelsonngaleumo@yahoo.fr>
2024-09-08 09:50:16 -0500

    homepage

--- pages/index.js
+++ pages/index.js
@@ -24,26 +24,38 @@ function Home() {
         overflow: 'hidden',
       }}
     >
+      <div>
+        <h1>Hello {user.displayName}!</h1>
+        <h3>Welcome to MamizaHair Store</h3>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
       <div style={{ position: 'relative', marginTop: '-100px' }}>
         <Image
           src="/MamizaHairLogo.jpg"
           alt="MAMIZA HAIR Logo"
           style={{
-            height: '400px',
+            height: '200px',
             display: 'block',
             margin: '0 auto',
           }}
         />
       </div>
       <div style={{ marginTop: '-100px' }}>
-        <h1>Hello {user.displayName}!</h1>
-        <h2>Welcome to MamizaHair Store</h2>
-        <p>Click the button below to logout!</p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         <h3>SELECT A CATEGORY</h3>
         <Button className="d-block w-100 mb-2" onClick={() => navigateTo('/HaircareProducts')}>Haircare</Button>
         <Button className="d-block w-100 mb-2" onClick={() => navigateTo('/AfroProducts')}>Afro</Button>
         <Button className="d-block w-100 mb-2" onClick={() => navigateTo('/SkincareProducts')}>Skincare</Button>
         <Button className="d-block w-100 mb-2" onClick={() => navigateTo('/cart')}>View Cart</Button>
+        <p>Click the button below to logout!</p>
         <Button className="d-block w-100" onClick={signOut}>Sign Out</Button>
       </div>
     </div>
